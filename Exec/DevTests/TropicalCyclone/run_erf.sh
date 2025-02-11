4ad7d7113ae3ff334fc4b0651987f6be023994af
Miguel Sanchez Gomez <mgomez@kl3.head.cm.kestrel.hpc.nrel.gov>
2025-02-11 14:56:08 -0700

    commit old stuff

---
+++ Exec/DevTests/TropicalCyclone/run_erf.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=90
+#SBATCH --time=01:00:00
+#SBATCH --output=log.erf.%j 
+#SBATCH --account=storm
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user="msanche2@nrel.gov"
+#SBATCH --job-name=test_erf
+#SBATCH --partition=debug
+#SBATCH --export=ALL
+#SBATCH --exclusive=user
+#SBATCH --mem=205000
+
+source ~/.erf_cpu_kestrel
+
+srun ./ERF.ex ./inputs_eliot
+
+exit 0
